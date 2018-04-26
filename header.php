8ee7628ee13ffeb105b6bb111b5bdbcd47870052
Cole <cmulli@iastate.edu>
2018-04-25 20:50:37 -0500

    General Formatting

---
+++ header.php
@@ -0,0 +1,34 @@
+
+<div id = "header">
+
+  <span id="logo"></span>
+  <div class="banner">
+    <div id="bannerimage">
+      <a href="Compare4care.html"><img src="Compare4carelogo.png"></a>
+    </div>
+    <div id="login">
+      <form>
+        <div class = "loginUsername">
+          <label for="username">Username:</label>
+          <input type="text" id="username" name="username">
+        </div>
+        <div class="loginPassword">
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password">
+      </div>
+        <div id="lower">
+          <input type="checkbox"><label for="checkbox">Keep me logged in</label>
+          <input type="submit" value="Login">
+        </div>
+      </form>
+    </div>
+  </div>
+  <div id="nav">
+       <p style="display:block">
+         <a href="compare.php">Compare</a>
+         <a href="about.php">About Us</a>
+         <a href="database.php">Database</a>
+          <a href="submitInfo.php">Submit Info</a></p>
+     </div>
+
+</div>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title> Compare4Care.org</title>
+  <link href="compare4care.css" rel="stylesheet" type "text/css">
+
+</head>
+
+  <body>
+    <?php include_once('header.php');?>
+    <div id="Surgerybanner">
+      <div id="typeofsurg">
+        <div class="optype">Select type of Operation:</div>
+        <select id="Typeofsurgery">
+        <option value = "1">Rhinoplasty</option>
+        <option value = "2">Hip replacement</option>
+        <option value = "3">Bowtox</option>
+        <option value = "4">Breast Reconstruction</option>
+	    	<option value = "5">Kidney Transplant</option>
+	    	<option value = "6">Knee replacement</option>
+    </select>
+      </div>
+      <br />
+      <div id="typeofinsur">
+        <div class="insurtype">Select Insurance Plan</div>
+          <select id="Insuranceplans">
+            <option value = "1">Blue Cross Blue Shield</option>
+            <option value = "2">United HealthGroup</option>
+            <option value = "3">Wellpoint</option>
+            <option value = "4">Humana</option>
+		        <option value = "5">Highmark</option>
+	          <option value = "6">Cigna Health</option>
+          </select>
+      </div>
+    </div>
+    <div class="mapouter">
+      <div class="gmap_canvas">
+        <iframe width="2000px" height="1000" id="gmap_canvas" src="https://maps.google.com/maps?q=Iowa City&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+        </iframe>
+      </div>
+    </div>
+    <p style="clear:both"><br></p>
+  </body>
+
+  </html>
