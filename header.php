55f334882864124ad162b2637bddb7efd2adc7b7
Cole <cmulli@iastate.edu>
2018-04-26 02:56:30 -0500

    Functionality, boxes, and jquery

--- header.php
+++ header.php
@@ -1,4 +1,29 @@
+<!DOCTYPE html>
+<html>
 
+<head>
+  <meta charset="UTF-8">
+  <title> Compare4Care.org</title>
+  <link href="compare4care.css" rel="stylesheet" type "text/css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+</head>
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script>$(document).ready(function() {
+	// get current URL path and assign 'active' class
+	var pathname = window.location.pathname;
+	$('.navbar-nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+})
+</script>
+<script>
+$("#Insuranceprov").change(function(){
+    if($("#Insuranceprov").val != "0"){
+      $('#Insuranceplans').attr("disabled", false);
+    }
+    else{
+        $('#Insuranceplans').attr("disabled", true);
+    }
+    });</script>
+  <body>
 <div id = "header">
 
   <span id="logo"></span>
@@ -23,12 +48,27 @@
       </form>
     </div>
   </div>
-  <div id="nav">
-       <p style="display:block">
-         <a href="compare.php">Compare</a>
-         <a href="about.php">About Us</a>
-         <a href="database.php">Database</a>
-          <a href="submitInfo.php">Submit Info</a></p>
-     </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-comp-red">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="/" >Home<span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about.php">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/database.php">Database</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/submitInfo.php">Submit Info</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
 
 </div>
--- index.php
+++ index.php
@@ -1,40 +1,69 @@
-<!DOCTYPE html>
-<html>
 
-<head>
-  <meta charset="UTF-8">
-  <title> Compare4Care.org</title>
-  <link href="compare4care.css" rel="stylesheet" type "text/css">
-
-</head>
-
-  <body>
     <?php include_once('header.php');?>
-    <div id="Surgerybanner">
-      <div id="typeofsurg">
-        <div class="optype">Select type of Operation:</div>
-        <select id="Typeofsurgery">
-        <option value = "1">Rhinoplasty</option>
-        <option value = "2">Hip replacement</option>
-        <option value = "3">Bowtox</option>
-        <option value = "4">Breast Reconstruction</option>
-	    	<option value = "5">Kidney Transplant</option>
-	    	<option value = "6">Knee replacement</option>
-    </select>
+
+      <div id="Surgerybanner">
+        <div class="row">
+          <div class="col-3"></div>
+          <div class="col-3">
+            <div id="typeofsurg">
+              <div class="optype">Select type of Operation:</div>
+              <select id="Typeofsurgery">
+                <option value = "0"></option>
+                <option value = "1">Rhinoplasty</option>
+                <option value = "2">Hip replacement</option>
+                <option value = "3">Bowtox</option>
+                <option value = "4">Breast Reconstruction</option>
+        	    	<option value = "5">Kidney Transplant</option>
+        	    	<option value = "6">Knee replacement</option>
+              </select>
+            </div>
+          </div>
+          <div class="col-3">
+            <div id="typeofinsur">
+              <div class="insurtype">Provider</div>
+              <select id="Insuranceprov">
+                <option value = "0"></option>
+                <option value = "1">Blue Cross Blue Shield</option>
+                <option value = "2">United HealthGroup</option>
+                <option value = "3">Wellpoint</option>
+                <option value = "4">Humana</option>
+    		        <option value = "5">Highmark</option>
+    	          <option value = "6">Cigna Health</option>
+              </select>
+            </div>
+        </div>
+        <div class="col-3"> </div>
       </div>
       <br />
-      <div id="typeofinsur">
-        <div class="insurtype">Select Insurance Plan</div>
-          <select id="Insuranceplans">
-            <option value = "1">Blue Cross Blue Shield</option>
-            <option value = "2">United HealthGroup</option>
-            <option value = "3">Wellpoint</option>
-            <option value = "4">Humana</option>
-		        <option value = "5">Highmark</option>
-	          <option value = "6">Cigna Health</option>
-          </select>
+      <div class="row">
+        <div class="col-3"></div>
+        <div class="col-3">
+          <div id="location">
+            <div class="optype">Your Location:</div>
+            <div id="locationField">
+              <input id="autocomplete" placeholder="Enter your address" onFocus="geolocate()" type="text"></input>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <div id="typeofinsur">
+            <div class="insurtype">Select Insurance Plan</div>
+            <select id="Insuranceplans" disabled>
+              <option value = "0"></option>
+              <option value = "1">Blue Cross Blue Shield</option>
+              <option value = "2">United HealthGroup</option>
+              <option value = "3">Wellpoint</option>
+              <option value = "4">Humana</option>
+              <option value = "5">Highmark</option>
+              <option value = "6">Cigna Health</option>
+            </select>
+          </div>
       </div>
+      <div class="col-3"> </div>
     </div>
+
+    </div>
+    <br />
     <div class="mapouter">
       <div class="gmap_canvas">
         <iframe width="2000px" height="1000" id="gmap_canvas" src="https://maps.google.com/maps?q=Iowa City&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
@@ -42,6 +71,41 @@
       </div>
     </div>
     <p style="clear:both"><br></p>
-  </body>
+    <script>function initAutocomplete() {
+        // Create the autocomplete object, restricting the search to geographical
+        // location types.
+        autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+            {types: ['geocode']});
 
-  </html>
+
+      }
+      function geolocate() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var geolocation = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            var circle = new google.maps.Circle({
+              center: geolocation,
+              radius: position.coords.accuracy
+            });
+            autocomplete.setBounds(circle.getBounds());
+          });
+        }
+      }
+      </script>
+      <script>
+      $("#Insuranceprov").change(function(){
+          if($("#Insuranceprov").val() != "0"){
+            $('#Insuranceplans').prop("disabled", false);
+          }
+          else{
+              $('#Insuranceplans').prop("disabled", true);
+              $("#Insuranceplans").val("0");
+          }
+          });</script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyClve9-lu-nJWthDhT17BgadylmCwkOx6k&libraries=places&callback=initAutocomplete"
+            async defer></script>
+  <?php include_once('footer.php');?>
